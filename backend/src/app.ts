f0d1eb1118864a1589319a385a6fc3f2d79f8769
hiqb269 <hiqb269@aucklanduni.ac.nz>
2026-03-31 19:46:18 +1300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (_req, res) => {
-  res.send('API is running');
+  res.send('API is running for Shimmering Shrews pretty app!');
 });
 
 app.use('/', testRoute);
